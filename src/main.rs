256bfd29b3747c17c1a98b14b1f8a2608cdb9a1a
Cong Luan Tran <congluan.tran19@gmail.com>
2025-05-14 14:11:03 +0700

    trial: basic TCP response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+
+    let message_size: i32 = 0;
+    let correlation_id: i32 = 7;
+    stream.write_all(&message_size.to_be_bytes())?;
+    stream.write_all(&correlation_id.to_be_bytes())?;
+    Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,8 +23,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                if let Err(e) = handle_connection(stream) {
+                    eprintln!("Failed to write to client: {e}");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
