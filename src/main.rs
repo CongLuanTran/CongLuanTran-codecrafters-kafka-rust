649441f4f022f7e5e7f54f0775c76791f4de5474
Cong Luan Tran <congluan.tran19@gmail.com>
2025-05-17 12:23:01 +0700

    trial: serial request by looping inside handle connection

--- src/main.rs
+++ src/main.rs
@@ -13,49 +13,58 @@ use codecrafters_kafka::protocol::{
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     // First read the message size
     let mut size_buf = [0; 4];
-    stream.read_exact(&mut size_buf)?;
-    let message_size = i32::from_be_bytes(size_buf) as usize;
+    loop {
+        match stream.read_exact(&mut size_buf) {
+            Ok(_) => {
+                let message_size = i32::from_be_bytes(size_buf) as usize;
 
-    // Then read the rest of the message into a buffer
-    let mut msg_buf = vec![0; message_size];
-    stream.read_exact(&mut msg_buf)?;
+                // Then read the rest of the message into a buffer
+                let mut msg_buf = vec![0; message_size];
+                stream.read_exact(&mut msg_buf)?;
 
-    // Parse the request
-    let request = RequestHeader::parse_header(msg_buf).unwrap();
+                // Parse the request
+                let request = RequestHeader::parse_header(msg_buf).unwrap();
 
-    // Send response, this is very unstructured for now, will be refactored later
-    let correlation_id: i32 = request.correlation_id;
+                // Send response, this is very unstructured for now, will be refactored later
+                let correlation_id: i32 = request.correlation_id;
 
-    if request.request_api_key == 18 {
-        let response_header = ResponseHeader { correlation_id };
-        let (error_code, api_keys) = match request.request_api_version {
-            0..=4 => (
-                0,
-                vec![ApiVersion {
-                    api_key: 18,
-                    min_version: 0,
-                    max_version: 4,
-                    tag_buffer: None,
-                }],
-            ),
-            _ => (35, vec![]),
-        };
+                if request.request_api_key == 18 {
+                    let response_header = ResponseHeader { correlation_id };
+                    let (error_code, api_keys) = match request.request_api_version {
+                        0..=4 => (
+                            0,
+                            vec![ApiVersion {
+                                api_key: 18,
+                                min_version: 0,
+                                max_version: 4,
+                                tag_buffer: None,
+                            }],
+                        ),
+                        _ => (35, vec![]),
+                    };
 
-        let response_body = ApiVersionResponse {
-            error_code,
-            api_keys,
-            throttle_time_ms: 0,
-            tag_buffer: None,
-        };
-        let response = Response {
-            header: response_header,
-            body: response_body,
-        };
+                    let response_body = ApiVersionResponse {
+                        error_code,
+                        api_keys,
+                        throttle_time_ms: 0,
+                        tag_buffer: None,
+                    };
+                    let response = Response {
+                        header: response_header,
+                        body: response_body,
+                    };
 
-        let payload = response.to_be_bytes();
-        let message_size: i32 = payload.len() as i32;
-        stream.write_all(&message_size.to_be_bytes())?;
-        stream.write_all(&payload)?;
+                    let payload = response.to_be_bytes();
+                    let message_size: i32 = payload.len() as i32;
+                    stream.write_all(&message_size.to_be_bytes())?;
+                    stream.write_all(&payload)?;
+                }
+            }
+            Err(e) => {
+                println!("Connection closed or error: {e}");
+                break;
+            }
+        }
     }
     Ok(())
 }
